e16d04a8321cd7afb291062efba4678dd8ea7317
Michael Aular <michaelaular@gmail.com>
2023-11-01 15:07:33 +0100

    update

--- src/pages/CompanyPage.js
+++ src/pages/CompanyPage.js
@@ -1,47 +1,28 @@
-import { useParams } from 'react-router';
-import { useEffect, useState } from 'react';
-import { getCompany } from '../lib/graphql/queries';
-import JobList from '../components/JobList';
+import { useParams } from "react-router";
+import { companyByIdQuery } from "../lib/graphql/queries";
+import JobList from "../components/JobList";
+import { useQuery } from "@apollo/client";
 
 function CompanyPage() {
   const { companyId } = useParams();
-  const [state, setState] = useState({
-    company: null,
-    loading: true,
-    error: false,
+  const { data, loading, error } = useQuery(companyByIdQuery, {
+    variables: { id: companyId },
   });
-  useEffect(()=>{
-    (async () => {
-      try {
-        const company = await getCompany(companyId);
-        setState({company, loading: false, error: false})
-      } catch (error) {
-        console.log('error:', JSON.stringify(error, null, 2))
-        setState({company: null, loading: false, error: true})
-      }
-    })();
-  },[companyId])
 
-  console.log('[CompanyPage] company:', state);
-  const {company, loading, error} = state;
+  console.log("[CompanyPage]:", { data, loading, error });
   if (loading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
   if (error) {
-    return <div className='has-text-danger'>Data unavailable</div>
+    return <div className="has-text-danger">Data unavailable</div>;
   }
+  const { company } = data;
   return (
     <div>
-      <h1 className="title">
-        {company.name}
-      </h1>
-      <div className="box">
-        {company.description}
-      </div>
-      <h2 className='title is-5'>
-        Jobs at {company.name}
-      </h2>
-      <JobList jobs={company.jobs}/>
+      <h1 className="title">{company.name}</h1>
+      <div className="box">{company.description}</div>
+      <h2 className="title is-5">Jobs at {company.name}</h2>
+      <JobList jobs={company.jobs} />
     </div>
   );
 }
